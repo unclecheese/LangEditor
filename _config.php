29e240e4e3d5b64087ff5257218218a443f8c5d2
Donatas <donatas@navidonskis.com>
2016-11-18 16:33:10 +0200

    css and js

--- _config.php
+++ _config.php
@@ -3,6 +3,9 @@
 define('LANGEDITOR_BASE', basename(dirname(__FILE__)));
 
 Config::inst()->update('LeftAndMain', 'extra_requirements_javascript', [
-    LANGEDITOR_BASE.'/javascript/lang_editor.js' => [],
-    LANGEDITOR_BASE.'/css/lang_editor.css'       => [],
+    LANGEDITOR_BASE.'/javascript/lang_editor.js',
+]);
+
+Config::inst()->update('LeftAndMain', 'extra_requirements_css', [
+    LANGEDITOR_BASE.'/css/lang_editor.css',
 ]);
